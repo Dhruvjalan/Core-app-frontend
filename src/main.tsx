75041396bd6ec408881191fd90a4d3dc217bb1d1
Dhruv Jalan <dhruvjalan0202@gmail.com>
2026-03-26 21:07:54 +0530

    ready to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-// clientid: 493222136166-1r1qtntlnjnvv7212ckiquu4tsv0838o.apps.googleusercontent.com
-// client secret; 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
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 
 
